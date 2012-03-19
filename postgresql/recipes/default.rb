17aa3e71de3f514b339edfdea4de7aaeff46af6b
Madhavan <madhuupri@gmail.com>
2012-03-19 16:52:06 +0530

    postgresql installation & configurations

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -28,16 +28,16 @@ end
 
 template "#{node[:postgresql][:dir]}/pg_hba.conf" do
   source "pg_hba.conf.erb"
-  owner "postgres"
-  group "postgres"
+  owner "root"
+  group "root"
   mode 0600
   notifies :reload, resources(:service => "postgresql")
 end
 
 template "#{node[:postgresql][:dir]}/postgresql.conf" do
   source "postgresql.conf.erb"
-  owner "postgres"
-  group "postgres"
+  owner "root"
+  group "root"
   mode 0600
   notifies :restart, resources(:service => "postgresql")
 end
